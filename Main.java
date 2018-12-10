a447ce377dd8bad18763224fa86327dfef5a973a
kishorebolt03 <45748742+kishorebolt03@users.noreply.github.com>
2018-12-10 13:52:08 +0530

    Update Main.java
    updated at 1:51 pm 10/12/18

--- Main.java
+++ Main.java
@@ -1,26 +1,26 @@
 import java.io.*;
 import java.util.*;
-
+//Program to check wether sum of any 2 elements in the given array are equal to sum of rest of the elements in the array
+//Main is the class  and i hav used only one class and a main method to complete the code 
 public class Main{
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int arr[]=new int[100];
+		Scanner s=new Scanner(System.in); 					//Scanner class is imported to get input of the elements and the number of elements
+		int arr[]=new int[100];							//Initializing variables 
 		int n=s.nextInt();
 		int m=0,flag=0;
 		int sum=0;
 		for(int i=0;i<n;i++){
-			arr[i]=s.nextInt();
+			arr[i]=s.nextInt();						// getting elements in array
 		}
 		for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
+            for(int j=i+1;j<n;j++){							//  
                 m=arr[i]+arr[j];
                 sum=0;
                 for(int k=0;((k<n));k++){
-					if( (k!=i) && (k!=j))
-					{
-						
-                    sum+=arr[k];
-					}
+			if( (k!=i) && (k!=j))
+			{
+			          sum+=arr[k];
+				}
                 }
                 if(m==sum){
                     flag=1;
